cc60fcadbec6065781b174abf0816f70419544cd
Nico <Nico@172.16.232.170>
2014-05-09 14:39:41 +0200

    Hausaufgabe vom 09.05.14

---
+++ GY12/src/gy12/ModuloDiv.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gy12;
+
+/**
+ *
+ * @author Nico
+ */
+public class ModuloDiv {
+    public static void main(String [] args){
+System.out.println(17 / 3);
+System.out.println(17 % 3);
+//System.out.println(17 / (1-1));
+ }
+    
+}
---
+++ GY12/src/gy12/Ueberlauf.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gy12;
+
+/**
+ *
+ * @author Nico
+ */
+public class Ueberlauf {
+    public static void main(String [] args){
+ System.out.println(Integer.MAX_VALUE);
+ System.out.println(Integer.MAX_VALUE+1);
+ }
+    
+}
