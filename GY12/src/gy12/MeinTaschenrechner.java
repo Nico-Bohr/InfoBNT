8b86acfbfb39d4649729bf8e793a26bb631fe5a4
Nico <Nico@172.16.232.170>
2014-05-23 13:23:40 +0200

    Projektarbeit | Taschenrechner | Bohr und Behrens

--- GY12/src/gy12/MeinTaschenrechner.java
+++ GY12/src/gy12/MeinTaschenrechner.java
@@ -10,34 +10,52 @@ public class MeinTaschenrechner {
     
     public static void main(String[]args){
         
-        double var1 = 0;
-        double var2 = 0;
-        double erg = 0;
         
-        String i = null;
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Geben Sie einen Wert für Variable 1 ein:");
-        var1= input.nextDouble();
-        
-        System.out.println("Geben Sie einen Wert für Variable 2 ein:");
-        var2= input.nextDouble();
-        
-        System.out.println("Geben sie 'a' für Addition, 's' für Substraktion, 'm' für Multiplikation und 'd' für Division ein");
-        i = input.next();
-        
-        char choice = i.charAt(0);
-        
-        switch(choice)
+        boolean var=true;
+        while(var)
         {
-            case 'a' : erg= var1+var2; break;
-            case 's' : erg= var1-var2; break;
-            case 'm' : erg= var1*var2; break;
-            case 'd' : erg= var1/var2;
-      
-        }
         
-        System.out.println("Ergebniss lautet:" +erg);
+            double var1 = 0;
+            double var2 = 0;
+            double erg = 0;
+
+            String i = null;
+            Scanner input = new Scanner(System.in);
+            
+        
+            System.out.println("Geben Sie einen Wert für Variable 1 ein:");
+            var1= input.nextDouble();
+
+            System.out.println("Geben Sie einen Wert für Variable 2 ein:");
+            var2= input.nextDouble();
+
+            System.out.println("Geben sie 'a' für Addition, 's' für Substraktion, 'm' für Multiplikation und 'd' für Division ein");
+            i = input.next();
+
+            char choice = i.charAt(0);
+
+            switch(choice)
+            {
+                case 'a' : erg= var1+var2; break;
+                case 's' : erg= var1-var2; break;
+                case 'm' : erg= var1*var2; break;
+                case 'd' : erg= var1/var2; break;
+
+            }
+
+            System.out.println("Ergebniss lautet:" +erg);
+            
+            
+            System.out.println("Um das Programm abzubrechen drücken Sie 'e' und um fortzusetzen drücken Sie 'c'");
+             i = input.next();
+            
+            char choice2 = i.charAt(0);
+            switch(choice2)
+                {
+                case 'c' : var=true; break;
+                case 'e' : var=false; 
+            }
+        }
     }
     
 }
